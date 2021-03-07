5da6152887e99665029a81d3c04c9957a79d44e7
Rob Arthur <rarthur@lightkeeper.com>
2021-03-07 16:20:01 -0500

    Added Route53, ALB, Cognito resources

--- main.tf
+++ main.tf
@@ -111,6 +111,8 @@ resource "aws_instance" "web_server" {
                   sudo su
                   yum -y install httpd
                   echo "<p> Hello World! </p>" >> /var/www/html/index.html
+                  echo "<p> Super Secure </p>" >> /var/www/html/secure/index.html
+                  echo "<p> Hello World! </p>" >> /var/www/html/open/index.html
                   sudo systemctl enable httpd
                   sudo systemctl start httpd
                   EOF
@@ -127,27 +129,29 @@ resource "aws_lb" "alb" {
   subnets            = module.vpc.public_subnets
 }
 
-resource "aws_lb_listener" "alb_listener" {
+resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.dev_robarthur_co_uk.arn
   default_action {
-    target_group_arn = aws_lb_target_group.alb_target.arn
-    type             = "forward"
-  }
-}
-
-resource "aws_lb_listener_rule" "listener_rule" {
-  listener_arn = aws_lb_listener.alb_listener.arn
-
-  action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.alb_target.id
   }
+}
 
-  condition {
-    path_pattern {
-      values = ["*"]
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
@@ -172,4 +176,108 @@ resource "aws_lb_target_group_attachment" "web_server_attachment" {
   target_group_arn = aws_lb_target_group.alb_target.arn
   target_id        = aws_instance.web_server.id
   port             = 80
+}
+
+
+#
+# TLS Certificate
+#
+
+data "aws_route53_zone" "robarthur_co_uk" {
+  name = "robarthur.co.uk."
+}
+
+resource "aws_acm_certificate" "dev_robarthur_co_uk" {
+  domain_name = "robarthur.co.uk"
+  subject_alternative_names = ["dev.robarthur.co.uk", "auth.robarthur.co.uk"]
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "dev_robarthur_co_uk" {
+  for_each = {
+    for dvo in aws_acm_certificate.dev_robarthur_co_uk.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.robarthur_co_uk.zone_id
+}
+
+resource "aws_acm_certificate_validation" "dev_robarthur_co_uk" {
+  certificate_arn         = aws_acm_certificate.dev_robarthur_co_uk.arn
+  validation_record_fqdns = [for record in aws_route53_record.dev_robarthur_co_uk : record.fqdn]
+}
+
+resource "aws_route53_record" "dev_robarthur_co_uk_a" {
+  zone_id = data.aws_route53_zone.robarthur_co_uk.zone_id
+  name    = "dev"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
+  }
+}
+
+
+#
+# Cognito Resources
+#
+
+resource "aws_cognito_user_pool" "user_pool" {
+  name = "test-user-pool"
+}
+
+resource "aws_cognito_user_pool_domain" "user_pool_domain" {
+  domain          = "auth.robarthur.co.uk"
+  certificate_arn   = aws_acm_certificate.dev_robarthur_co_uk.arn
+  user_pool_id = aws_cognito_user_pool.user_pool.id
+}
+
+resource "aws_cognito_user_pool_client" "user_pool_client" {
+  name                                 = "test-user-pool-client"
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_scopes                 = ["openid"]
+  callback_urls                        = ["https://dev.robarthur.co.uk"]
+  generate_secret                      = true
+  supported_identity_providers         = ["COGNITO"]
+  user_pool_id                         = aws_cognito_user_pool.user_pool.id
+}
+
+resource "aws_lb_listener_rule" "secure_listener_rule" {
+
+  listener_arn = aws_lb_listener.https.arn
+
+  action {
+    type = "authenticate-cognito"
+
+    authenticate_cognito {
+      scope               = "openid"
+      user_pool_arn       = aws_cognito_user_pool.user_pool.arn
+      user_pool_client_id = aws_cognito_user_pool_client.user_pool_client.id
+      user_pool_domain    = aws_cognito_user_pool_domain.user_pool_domain.id
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target.id
+  }
+
+  condition {
+    path_pattern {
+      values = ["/secure/*"]
+    }
+  }
 }
